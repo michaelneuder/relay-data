511659b34e0aa8c7dd97d6d3f4595a0ca5f96bdb
Michael Neuder <mikeneuder@google.com>
2023-02-28 15:12:25 -0500

    windelta

--- windelta/windelta.sh
+++ windelta/windelta.sh
@@ -4,6 +4,7 @@ do
     SLOT=$(printf "%d\n" ${line[0]})
     HASH=${line[1]}
     COMMAND="SELECT slot,value,substring(block_hash,0,9) as hash,inserted_at,received_at FROM mainnet_builder_block_submission WHERE slot=$SLOT AND block_hash~'$HASH' ORDER BY inserted_at ASC LIMIT 1;"
+    # COMMAND2="SELECT slot,value,substring(block_hash,0,9) as hash,inserted_at,received_at FROM mainnet_builder_block_submission WHERE slot=$SLOT ORDER BY value DESC LIMIT 1;"
     RES=$(psql -p 5433 "host=0.0.0.0 sslmode=disable dbname=defaultdb user=readonly" -t -c "$COMMAND")
     echo $RES
-done < winninghashslots.txt
+done < winninghashslots2.txt
