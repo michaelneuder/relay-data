e3534a4b65583a69ddc31fea148408e686468fe8
Michael Neuder <mikeneuder@google.com>
2023-03-22 09:20:05 -0400

    winning flow duration

--- winningflow/winning.sh
+++ winningflow/winning.sh
@@ -3,7 +3,7 @@ do
     BN=$(printf "%d\n" ${line[0]})
     HASH=${line[1]}
 
-    COMMAND2="SELECT builder_pubkey,received_at,eligible_at FROM mainnet_builder_block_submission WHERE block_number=$BN AND block_hash='$HASH' ORDER BY received_at ASC LIMIT 1;"
+    COMMAND2="SELECT builder_pubkey,received_at,eligible_at,optimistic_submission,total_duration FROM mainnet_builder_block_submission WHERE block_number=$BN AND block_hash='$HASH' ORDER BY received_at ASC LIMIT 1;"
     RES2=$(psql -p 5433 "host=0.0.0.0 sslmode=disable dbname=defaultdb user=readonly" -t -c "$COMMAND2")
     echo $BN "|" $HASH "|" $RES2
 
